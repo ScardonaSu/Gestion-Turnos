ad3d897ac45f32f0b2c6bcd0adce7a9b1d750bff
Santo <santicardonal777@icloud.com>
2024-04-23 19:18:08 -0500

    conexion a mysql

--- GestionTurnos/Program.cs
+++ GestionTurnos/Program.cs
@@ -1,7 +1,19 @@
+using GestionTurnos.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+
+//conexión a la Db
+builder.Services.AddDbContext<BaseContext>(options =>
+    options.UseMySql(
+        builder.Configuration.GetConnectionString("MySqlConnection"),
+        Microsoft.EntityFrameworkCore.ServerVersion.Parse("8.0.20-mysql")));
+
+
 
 var app = builder.Build();
 

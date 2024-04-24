4545a46544084e8f122ab877e11c2ce1e96846bd
SantiagoMona <Santimona1@gmail.com>
2024-04-24 15:54:25 -0500

    add hash password

--- GestionTurnos/Controllers/AsesoresController.cs
+++ GestionTurnos/Controllers/AsesoresController.cs
@@ -1,5 +1,6 @@
 using GestionTurnos.Data;
 using GestionTurnos.Models;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
 namespace GestionTurnos.Controllers;
@@ -8,7 +9,8 @@ public class AsesoresController : Controller
 {
 
     public readonly BaseContext _Context;
-    
+    private object Password;
+
     public AsesoresController(BaseContext context)
     {
         _Context = context;
@@ -27,13 +29,16 @@ public class AsesoresController : Controller
     [HttpPost]
     public async Task<IActionResult> Create(Asesor _asesor)
     {
+        Asesor newAsesor = new Asesor{
+            
+            Password = BCrypt.Net.BCrypt.HashPassword(_asesor.Password)
+
+        };
+        _asesor.Password = newAsesor.Password;
+
         _Context.Asesores.Add(_asesor);
         await _Context.SaveChangesAsync();
         return RedirectToAction("Login", "Asesores");
     }
-    
-    
-
-
 
 }

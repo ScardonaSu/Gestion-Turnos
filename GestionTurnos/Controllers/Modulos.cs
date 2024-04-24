0421342bc523f093177d6a24ff3df988a8e26988
Santo <santicardonal777@icloud.com>
2024-04-23 19:56:30 -0500

    Modulos agregados

---
+++ GestionTurnos/Controllers/Modulos.cs
@@ -0,0 +1,10 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace GestionTurnos.Controllers;
+
+public class Modulos : Controller
+{
+    
+    
+    
+}
---
+++ GestionTurnos/Models/Modulo.cs
@@ -0,0 +1,8 @@
+namespace GestionTurnos.Models;
+
+public class Modulo
+{
+    public int Id { get; set;}
+    
+    public string Gestion { get; set; }
+}

fe75945efc96576d3a434d2b5725932effbdb451
KennyVell <kenneth.geme1@gmail.com>
2024-04-25 21:20:17 -0500

    turnero

--- GestionTurnos/Controllers/TurnosController.cs
+++ GestionTurnos/Controllers/TurnosController.cs
@@ -16,10 +16,55 @@ public class TurnosController : Controller {
     }
 
     public async Task<IActionResult> SeleccionarCategoria (string documento) {
-        HttpContext.Session.SetString("Documento",documento);
+        if(!string.IsNullOrEmpty(documento)){
+            HttpContext.Session.SetString("Documento",documento);
+            ViewBag.Documento = HttpContext.Session.GetString("Documento");
+            return View(await _context.Categorias.ToArrayAsync());
+        }
+
+        return RedirectToAction("Index","Turnos");
+    }
+
+    public async Task<IActionResult> Turno(string siglas){
 
         ViewBag.Documento = HttpContext.Session.GetString("Documento");
 
-        return View(await _context.Categorias.ToArrayAsync());
+        string seleccion = "";
+        string turno = "";
+        var result = await _context.Categorias.FirstOrDefaultAsync(c => c.Abreviacion == siglas);
+        int contador = result.Contador +1;
+
+        turno = siglas+"-"+(contador < 10 ? "00"+contador: "0"+contador);
+
+        ViewBag.Turno = turno;
+        return View();
+    }
+
+    public async Task<IActionResult> TomarTurno(string turno){
+
+        string documento = HttpContext.Session.GetString("Documento");
+
+        string siglas = turno.Substring(0,2);
+        var result = await _context.Categorias.FirstOrDefaultAsync(c => c.Abreviacion == siglas);
+
+        result.Contador = result.Contador+1; 
+
+        _context.Categorias.Update(result);
+        await _context.SaveChangesAsync();
+
+        var turnoIn = new Turno{
+            Categoria = result.Nombre,
+            Documento = documento,
+            Ticket = turno,
+            FechaTicket = DateTime.Now,
+            Proceso = "En cola"
+
+        };
+
+        _context.Turnos.Add(turnoIn);
+        await _context.SaveChangesAsync();
+
+
+        return RedirectToAction("Index");
     }
 }
